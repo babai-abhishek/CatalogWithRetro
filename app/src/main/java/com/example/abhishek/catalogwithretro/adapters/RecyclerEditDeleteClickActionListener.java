f57b035725679b43391ae70be952e177a5187200
abhishek <babbi.abhishek@gmail.com>
2020-05-21 12:27:27 +0530

    unwanted lines removed

--- app/src/main/java/com/example/abhishek/catalogwithretro/adapters/RecyclerEditDeleteClickActionListener.java
+++ app/src/main/java/com/example/abhishek/catalogwithretro/adapters/RecyclerEditDeleteClickActionListener.java
@@ -2,9 +2,6 @@ package com.example.abhishek.catalogwithretro.adapters;
 
 import android.view.View;
 
-/**
- * Created by abhishek on 10/11/17.
- */
 
 public interface RecyclerEditDeleteClickActionListener {
     void onAction(int position, int action);
